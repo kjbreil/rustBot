a2bd262441c7f55e03917be1e871383bce81afbb
Kjell Breiland <kjell.breiland@gmail.com>
2016-09-20 18:44:28 -0700

    Read Log file - first matcher

---
+++ matchers/clientData.js
@@ -0,0 +1,7 @@
+// clientData.js
+
+// Template file for rustBot matchers
+
+// Set the regex matching, must be same name as file
+
+clientData  = new RegExp(/^([0-9.]{7,15})\:([0-9]*)\/([0-9]*)\/(.*)/);
