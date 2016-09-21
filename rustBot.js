3d7c7b4e6c0298d4200a20347cf60c432c058236
Kjell Breiland <kjell.breiland@gmail.com>
2016-09-20 18:46:59 -0700

    add readline and console output

--- rustBot.js
+++ rustBot.js
@@ -1,1 +1,51 @@
-#!/usr/bin/js
+#!/usr/bin
+
+const fs = require("fs");
+
+var inputlogfile = "./exLog/gamelog-2016-09-17-03-00-01.log";
+
+
+function log(msg, color) {
+	//rustify the date
+    var date = new Date().toISOString()
+                            .replace(/T/, ' ')
+                            .replace(/\..+/, '');
+    var text = msg;
+    
+    process.stdout.write('['+date+'] ' + text + '\n');
+    //log file, should already be created
+    fs.appendFile('rcon.log', '['+date+'] ' + text + '\n');
+}
+
+
+
+
+
+// Rust log file date format (why am i keeping this???)
+var date = new Date().toISOString()
+                        .replace(/T/, ' ')
+                        .replace(/\..+/, '');
+//set the variables for the filedate format, YYYYMMDD_HHMM
+var rightnow = new Date(),
+	year = rightnow.getFullYear(),
+	month = rightnow.getMonth(),
+	day = rightnow.getDay()
+	hour = rightnow.getHours()
+	minute = rightnow.getMinutes();
+var fileDate = String(year) + String(month) + String(day) + '_' + String(hour) + String(minute);
+//Rename the old log file - fuck this for right now, will be implemented when live
+// fs.rename('rcon.log',fileDate + '_rcon.log');
+fs.writeFile('rcon.log','['+date+'] ' + 'RCON SCRIPT STARTED' + '\n');
+
+
+var lineReader = require('readline').createInterface({
+  terminal: false, input: fs.createReadStream(inputlogfile)
+});
+
+
+
+lineReader.on('line', function (line) {
+  log(line);
+});
+
+
