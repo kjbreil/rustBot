3aa25df2e53bebf10e30f93a184faebab1afe9ae
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-02 19:21:29 -0700

    chat fixes

--- matchers/chat.js
+++ matchers/chat.js
@@ -7,10 +7,10 @@ chatRE = new RegExp(/\[CHAT\] (.+?)\[\d*\/(\d+)] : (.+)/);
 exports.chatIF = function (line) {
 	base.log(line, 1);
 	names = chatRE.exec(line)
-	base.log(names[1] + ': ' + names[3], 1, 'chat.log')
-	if(discordEnabled == 1) {discordMessage(names[1] + ': ' + names[3])}
+	base.log(names[1] + ': ' + names[3], 'lcd', 'chat.log', 'chat')
+	// if(discordEnabled == 1) {discordMessage(names[1] + ': ' + names[3], 'chat')}
 }
 
 exports.chatArray = function (msg){
-	base.log(msg.Message + ': ' + msg.Username, 'lcd')
+	base.log(msg.Message + ': ' + msg.Username, 'lcd', 'chat.log', 'chat')
 }
