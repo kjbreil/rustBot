8b63f9a3b2ec5d285091ceba87b60821d8a36a26
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-05 14:31:38 -0700

    I promise to use promises from now on

--- matchers/fps.js
+++ matchers/fps.js
@@ -6,5 +6,22 @@ const config = require('./../config.js');
 fpsRE = new RegExp(/^(\d{1,3}) FPS/)
 
 exports.fpsIF = function(msg) {
-	base.log('[***fps***] ' + msg, 'lcd', null, config.discordRooms.rcon)
+	msg = msg.Message
+	let dfpsRE = (/^\[.+\] \[\*\*\*fps\*\*\*\] \d{1,3} FPS/)
+    // discordDeleteMessageType(config.discordRooms.rcon, dfpsRE)
+    discordDeleteMessageType(config.discordRooms.rcon, dfpsRE).then(function (z) {
+		base.log('[***fps***] ' + msg, 'lcd', null, config.discordRooms.rcon)
+    })
+}
+
+ddmt = function(channel, type) {
+	return new Promise(function (resolve, reject) {
+	    channel = bot.channels.find('name', channel)
+	    channel.fetchMessages({limit : 100}).then(function (m) {
+	        filteredMessages = m.filter(findMessage.bind(this, type))
+	        filteredMessages.deleteAll()
+	    }).catch(function (err) {
+	        console.log('## ' + err);
+	    });
+    });
 }
