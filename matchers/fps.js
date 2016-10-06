268fc5093037af55a6864bd24777791ab8f8849d
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-06 10:53:11 -0700

    delete fps

--- matchers/fps.js
+++ matchers/fps.js
@@ -7,7 +7,7 @@ fpsRE = new RegExp(/^(\d{1,3}) FPS/)
 
 exports.fpsIF = function(msg) {
 	msg = msg.Message
-	let dfpsRE = (/^\[.+\]\[\*fps\*\:\*\*\*f\*\*\*\] (\d{1,3}) FPS/)
+	let dfpsRE = (/^\[.+\] \[\*fps\*\:\*\*\*f\*\*\*\] (\d{1,3}) FPS/)
     discordDeleteMessageType(config.discordRooms.rcon, dfpsRE).then(function (z) {
 		base.log('[*fps*:***f***] ' + msg, 'lcd', null, config.discordRooms.rcon)
     })
