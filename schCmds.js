83fdf0cbf5f7494fb01224066fff35bc62dfc718
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-06 08:27:28 -0700

    kill messages enabled again

--- schCmds.js
+++ schCmds.js
@@ -5,12 +5,6 @@ let playerListSCH = setInterval(function () {
     rcon.Command('global.playerlist', 'displayPlayers.discordDisplayPlayers'); 
 }, 60000); 
 
-// let playerListSCH2 = setInterval(function () { 
-//     rcon.Command('global.playerlist', 'displayPlayers.discordDisplayPlayers'); 
-// }, 9000); 
-
-
 exports.startSchCmds = function() {
 	playerListSCH
-	playerListSCH2
 }
