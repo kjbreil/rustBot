3e846de6c10563929b8fb23f8086ce6135d93567
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-06 20:15:58 -0700

    rcon hooked

--- rustBot.js
+++ rustBot.js
@@ -13,14 +13,28 @@ const fs = require('fs')
 var bot = require('auto-loader').load(__dirname +'/bot')
 var discord = require('auto-loader').load(__dirname +'/discord')
 var rcon = require('auto-loader').load(__dirname +'/rcon')
-// console.log(bot)
 
 bot.fsUtils.createDirectories()
 bot.fsUtils.renameLogFiles()
 
-
-
-
 global.rcon = new WebRcon(config.addr, config.port)
+
+rcon.on('connect', function() {
+    console.log('CONNECTED')
+    
+})
+rcon.on('disconnect', function() {
+    console.log('DISCONNECTED')
+})
+rcon.on('message', function(msg) {
+    console.log('MESSAGE:', msg)
+})
+rcon.on('error', function(err) {
+    console.log('ERROR:', err)
+})
+ 
+// Connect by providing the server's rcon.password: 
+rcon.connect(config.pass)
+
 
 rcon.connect(config.pass)
