ccc74abef90c54d3788056eb46d5bf82223e333a
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-09 13:28:29 -0700

    heli event and log on/off notification

--- rust/rustGeneric.js
+++ rust/rustGeneric.js
@@ -3,6 +3,7 @@
 
 let deathRE = new RegExp(/.*\[\d{1,5}\/\d{15,18}\]/)
 let eventRE = new RegExp(/^\[event\] (.+?) (.+?)$/)
+let clientRE  = new RegExp(/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,6})\/(\d+?)\/(.+)/);
 
 
 exports.rustGenericGate = function(msg) {
@@ -10,9 +11,12 @@ exports.rustGenericGate = function(msg) {
 	if(deathRE.test(msg.message)) {
 		rust.rustDeath.deathMessageIF(msg.message)
 	} else if (eventRE.test(msg.message)) { 
-		log('GenericEvent' + msg.message, 'l', logFile.rustbot, null)
+		rust.rustEvent.rustEventGate(msg.message)
+		log('GenericEvent ' + msg.message, 'l', logFile.rustbot, null)
+	} else if (clientRE.test(msg.message)) {
+		log(msg.message, 'ld', logFile.connect, discordRoom.log);
 	} else {
-		log('Generic Nothing' + msg.message, 'l', logFile.rustbot, null)
+		log('Generic Nothing ' + msg.message, 'l', logFile.rustbot, null)
 	}
 
 }
