b8cec532196fbce55d7a74c162eae278c5e2bba5
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-12 16:29:08 -0700

    bookshelf test

--- cpu/sql/sqlTest.js
+++ cpu/sql/sqlTest.js
@@ -10,69 +10,27 @@ let knexConnection = config.knex.connection
 
 global.knex = require('knex')(config.knex)
 
-// // datetime, ip, port, steamid, name, connect, os, os_steamid, disconnect, disconnect_why, auth, auth_level
+var Model = require('objection').Model
 
-// // knex.select('datetime', 'ip', 'port').from('connect_log').then(function (msg) {
-// // 	console.log(msg)
-// // })
-
-// var lineReader = require('line-reader');
-
-
-// var bookshelf = require('bookshelf')(knex);
-
-// var connectLog = bookshelf.Model.extend({
-//   tableName: 'connect_log'
-// }, {
-// 	bySteamID: function(steamid) {
-//         return this.forge().query({where:{ steamid: steamid }}).fetch();
-//     }
-// })
-
-// var connectLogs = bookshelf.Collection.extend({  
-//     model: connectLog
-// });
-
-// connectLog.fetchAll().fetch().then(function(connections) {
-// 	console.log(connections.get('steamid'))
-// })
-
-// // connectLog.bySteamID('76561198006975241')
-
-// // var bookshelf = require('bookshelf')(knex);
-
-// // var connect_log = bookshelf.Model.extend({
-// // 	tableName: 'connect_log',
-// // })
-// // datetime, ip, port, steamid, name, connect, os, os_steamid, disconnect, disconnect_why, auth, auth_level
-
-// startLineReader = function() {
-// 	lineReader.eachLine('merged.log', function(line, last) {
-// 		deathToSql(line).then(function(line) {
-// 			connectLogToSQL(line).then(function(line) {
-// 				if(line === '') {
-
-// 				} else if (line === '\n') {
-					
-// 				} else {
-// 					// console.log(line)
-// 				}
-				
-// 			}).catch(function(err) {
-
-// 			})
-// 		}).catch(function (err) {
-//             // console.log('skipped')
-//         })
-		
-// 	})
-// }
-
-//cpu.sql.
 const sqlSelects = require('./sqlSelects')
 
-sqlSelects.sqlSelectPlayerListView().then(function () {
-	console.log('promise')
-	process.exit()
+var bookshelf = require('bookshelf')(knex)
+
+var PlayerList = bookshelf.Model.extend({
+	tableName: 'connected_players'
 })
+
+PlayerList.fetchAll().then(function(PlayerList){
+	for(i in PlayerList.models) {
+		console.log(PlayerList.models[i].attributes)
+	}
+
+})
+
+
+
+// sqlSelects.sqlSelectPlayerListView().then(function () {
+// 	console.log('promise')
+// 	process.exit()
+// })
 
