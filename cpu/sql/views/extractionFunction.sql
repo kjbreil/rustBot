202ec4815577a1b82c59a227a20bb3293a736610
Kjell Breiland <kjell.breiland@gmail.com>
2016-10-19 06:46:37 -0700

    sql to save

--- cpu/sql/views/extractionFunction.sql
+++ cpu/sql/views/extractionFunction.sql
@@ -1,3 +1,65 @@
+select stats_extract(date '10/13/2016', date '10/16/2016');
+SELECT * FROM STATS;
+
+select stats_add(76561197986861802, date '2016-10-14 17:32:11.244969-07');
+
+SELECT SUM(value)
+	FROM stats
+	WHERE steamid = 76561197986861802
+	AND data_time = date '2016-10-14 17:32:11.244969-07';
+
+
+CREATE OR REPLACE FUNCTION stats_add(passsteamid int8, start_time date)
+RETURNS INTEGER AS $$
+DECLARE 
+	first_time date;
+	last_disconnect date;
+	
+BEGIN
+	return (
+	SELECT SUM(value)
+	FROM stats
+	WHERE steamid = passsteamid
+	AND data_time = start_time);
+
+
+END;
+$$ LANGUAGE plpgsql;
+
+ DROP FUNCTION stats_add(bigint,date) ;
+
+
+ INSERT INTO stats (data_time, connect, steamid, stat, value, running)
+
+SELECT
+	ssa.created_at,
+	ssa.connect,
+	ssa.steamid,
+	st.name,
+	st.value,
+	0 as running
+FROM
+	steamstats_audit ssa,
+	LATERAL jsonb_populate_recordset (NULL :: death_type, ssa.stats->'stats'::text) st
+	WHERE created_at BETWEEN date '10/13/2016' AND date '10/14/2016'
+	ORDER BY ssa.steamid, st.name, ssa.created_at
+	
+
+SELECT
+	ssa.steamid,
+	ssa.created_at,
+	st.name,
+	st.value
+
+FROM
+	steamstats_audit ssa,
+	LATERAL jsonb_populate_recordset (NULL :: death_type, ssa.stats->'stats'::text) st
+	WHERE created_at BETWEEN date '10/13/2016' AND date '10/14/2016'
+
+
+
+
+
 DROP TABLE "public"."stats" CASCADE;
 	CREATE TABLE "public"."stats" (
 	"id" integer DEFAULT nextval('connect_id_seq'::regclass) NOT NULL,
