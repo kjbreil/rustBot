093960e2a14b6c2afa75e3894f6247b3c2b93875
Kjell Breiland <kjell.breiland@gmail.com>
2017-12-05 13:54:54 -0800

    Use promise.each

--- cpu/fsUtils.js
+++ cpu/fsUtils.js
@@ -2,23 +2,36 @@
 
 const fs = require('fs')
 
-exports.createDirectories = function() {
-	for (let i in config.reDir) {
-		try {
-		    let stats = fs.lstatSync('.\/' + config.reDir[i])
-		    if (stats.isDirectory()) {}
-		} catch (e) { fs.mkdirSync('.\/' + config.reDir[i]) }
-	}
+var lstat = Promise.promisify(fs.lstat)
+
+var rename = Promise.promisify(fs.rename)
+
+exports.createDirectories = function () {
+  for (let i in config.reDir) {
+    lstat('./' + config.reDir[i])
+      .then(stats => {
+        // need to do something if its a file instead of directory
+        if (stats.isDirectory()) {
+        }
+      })
+      .catch(() => {
+        fs.mkdirSync('./' + config.reDir[i])
+      })
+  }
 }
 
-exports.renameLogFiles = function() {
-	for (let i in logFile) {
-		try {
-		    let stats = fs.lstatSync(config.logFileLocation + i + '.log')
-		    if (stats.isFile()) {
-		    	let fileDate = dateFormat(new Date(), "yyyymmdd_hhMMss")
-		    	fs.rename(config.logFileLocation + i + '.log',config.logFileLocation +  fileDate + '_' + i + '.log')
-		    }
-		} catch (e) {}
-	}
+exports.renameLogFiles = function () {
+  for (let i in logFile) {
+    lstat(config.logFileLocation + i + '.log')
+      .then(stats => {
+        if (stats !== undefined && stats.isFile()) {
+          let fileDate = dateFormat(new Date(), 'yyyymmdd_hhMMss')
+          rename(config.logFileLocation + i + '.log', config.logFileLocation + fileDate + '_' + i + '.log')
+            .then(() => {})
+            .catch(() => {})
+        }
+      }).catch(() => {
+
+      })
+  }
 }
