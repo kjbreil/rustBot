019f32d08f8f5b10e462e85c5231144d7b9158fd
Kjell Breiland <kjell.breiland@gmail.com>
2017-12-12 16:21:59 -0800

    standard js

--- rust/rustEvent.js
+++ rust/rustEvent.js
@@ -8,20 +8,20 @@
 
 let dT = 'lrod'
 
-exports.rustEventGate = function(msg) {
-	log('EVENT: ' + msg, 'lc', logFile.info, discordRoom.bot)
-	msg = RegExp(/^\[event\] (.+) (.+)$/).exec(msg)
-	if(msg[2] === 'helicopter/patrolhelicopter.prefab') {
-		var message = [
-			{'color' : 'default', 'text': 'get naked or kiss your ass goodbye, the '},
-			{'color' : 'red', 'text' : 'heli '},
-			{'color' : 'default', 'text' : 'is'},
-			{'color' : 'green', 'text' : 'here'}
-		]
-		log(message, dT, logFile.rcon, discordRoom.chat)
-	} else if (msg[2] === 'plane/cargo_plane.prefab') {
-		log('EVENT: AD', 'lc', logFile.info, discordRoom.bot)
-	} else {
-		log('event error' + msg, 'lc', logFile.info, discordRoom.bot)
-	}
+exports.rustEventGate = function (msg) {
+  log('EVENT: ' + msg, 'lc', logFile.info, discordRoom.bot)
+  msg = RegExp(/^\[event\] (.+) (.+)$/).exec(msg)
+  if (msg[2] === 'helicopter/patrolhelicopter.prefab') {
+    var message = [
+      {'color': 'default', 'text': 'get naked or kiss your ass goodbye, the '},
+      {'color': 'red', 'text': 'heli '},
+      {'color': 'default', 'text': 'is'},
+      {'color': 'green', 'text': 'here'}
+    ]
+    log(message, dT, logFile.rcon, discordRoom.chat)
+  } else if (msg[2] === 'plane/cargo_plane.prefab') {
+    log('EVENT: AD', 'lc', logFile.info, discordRoom.bot)
+  } else {
+    log('event error' + msg, 'lc', logFile.info, discordRoom.bot)
+  }
 }
